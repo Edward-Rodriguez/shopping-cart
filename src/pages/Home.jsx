7e201006dbb980643f0fbe4296a8aff23ab20d71
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2026-07-02 16:42:46 -0400

    Add background image, heading & link to shop to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,29 @@
+import { Link } from 'react-router';
+import styles from './Home.module.css';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const baseImageUrl =
+    'https://res.cloudinary.com/do89l9smc/image/upload/f_auto,q_auto,';
+  const imageId = '/v1783019844/guy-basabose-EoqOVrMgmSA-unsplash_k0bkij.jpg';
+  return (
+    <div className={styles.container}>
+      <img
+        src={`${baseImageUrl}w_800${imageId}`}
+        srcSet={`${baseImageUrl}w_400${imageId} 400w,
+                  ${baseImageUrl}w_800${imageId} 800w,
+                  ${baseImageUrl}w_1200${imageId} 1200w,
+                  ${baseImageUrl}w_1600${imageId} 1600w,
+                  `}
+        sizes='100vw'
+        alt='Brown beach umbrella on white sand beside turquoise water.'
+      />
+      <div className={styles.header}>
+        <h3 className={styles.subheading}>LIMITED TIME ONLY</h3>
+        <h2 className={styles.shopPhrase}>SUMMER SALE</h2>
+        <Link className={styles.link} to='/shop'>
+          EXPLORE THE COLLECTION
+        </Link>
+      </div>
+    </div>
+  );
 }
