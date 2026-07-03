ee31507209da6ed9cdea12bce12bc5c65e428b65
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2026-07-03 15:45:19 -0400

    adjust shop page styling

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,15 +1,14 @@
 import ProductCard from '../components/ProductCard/ProductCard';
 import { useOutletContext } from 'react-router';
+import styles from './Shop.module.css';
 
 export default function Shop() {
   const data = useOutletContext();
   return (
-    <div>
-      <div className='container'>
-        <ul className='cardContainer'>
-          {data && data.map((product) => <ProductCard product={product} />)}
-        </ul>
-      </div>
+    <div className={styles.shopContainer}>
+      <ul className={styles.productCardContainer}>
+        {data && data.map((product) => <ProductCard product={product} />)}
+      </ul>
     </div>
   );
 }
